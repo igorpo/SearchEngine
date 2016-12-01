a2c0129cc64e4a2f86b52ef7ec9ea9d0317365e7
Yagil <yagilbu@gmail.com>
2016-12-01 16:54:54 -0500

    Frontier wrapper interface and local frontier for testing

---
+++ crawler/src/Testing/LocalFrontier.java
@@ -0,0 +1,26 @@
+package Testing;
+
+import Frontier.FrontierWrapper;
+
+/**
+ * Created by YagilB on 01/12/2016.
+ */
+public class LocalFrontier implements FrontierWrapper {
+
+    // TODO: @igor
+
+    @Override
+    public String pop(String threadID) {
+        return null;
+    }
+
+    @Override
+    public void push(String URL, String threadID) {
+
+    }
+
+    @Override
+    public int size(String threadID) {
+        return 0;
+    }
+}
