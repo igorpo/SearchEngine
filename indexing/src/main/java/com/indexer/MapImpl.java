065608b4d76a26a9cffd71ffe17c1c2a20df7146
Kieraj Mumick <kmumick@seas.upenn.edu>
2016-12-17 03:57:34 -0500

    refactoring map for indexer

--- indexing/src/main/java/com/indexer/MapImpl.java
+++ indexing/src/main/java/com/indexer/MapImpl.java
@@ -72,114 +72,65 @@ public class MapImpl extends Mapper<LongWritable, Text, Text, Text> {
     @Override
     public void map(LongWritable key, Text value, Context c) throws IOException, InterruptedException {
 
-        /*
-        String url = value.toString();
-        if(url == null || url.equals("")) {
-//            word.set(url);
-//            output.collect(word, one);
-            return;
-        }
-        S3Object urlDoc = s3.getObject(new GetObjectRequest(bucketName, url));
-        if (urlDoc == null){
-//            word.set("BAD");
-//            output.collect(word, one);
-            return;
-        }
+        // If the document has no text, skip it.
+        if (value.toString().equals("")) { return; }
 
-        BufferedReader reader = new BufferedReader(new
-                InputStreamReader(urlDoc.getObjectContent()));
-
-        String line;
-        StringBuilder sb = new StringBuilder();
-        while((line = reader.readLine()) != null){
-            sb.append(line);
-
-        }
-
-
-        String document = Jsoup.parse(sb.toString()).text();
-        */
-
+        // Get the file url.
         String url = ((FileSplit) c.getInputSplit()).getPath().getName();
 
+        // Parse out the document and ensure that is valid.
         Document test = Jsoup.parse(value.toString());
-        if (test == null){
-            return;
-        }
+        if (test == null || test.body() == null) { return; }
+
+        // Remove any code from the HTML.
         test.select("script,jscript,style").remove();
-        String document;
-        if (test.body() != null) {
-            document = test.body().text();
-        } else {
-            return;
-        }
-        if (test.head() != null) {
-            document += test.head().text();
-        }
-        //Text word = new Text();
-        //Text textUrl = new Text();
-        //word.set(url);
-        //textUrl.set(value.toString());
-        //c.write(word, textUrl);
 
-
+        // Parse out the text from the document.
+        String document = test.body().text();
+        if (test.head() != null) { document += test.head().text(); }
         String[] words = document.toLowerCase().split("\\W+");
 
-        //TODO: If running too slow, might want to get rid of stemming or extract to another for loop to stem only once
+        // TODO: If running too slow, might want to get rid of stemming or
+        // extract to another for loop to stem only once
         Map<String, Integer> tfs = new HashMap<>();
         String stemmedWord;
-        for (String w : words){
+        for (String w : words) {
             Stemmer stemmer = new Stemmer();
             stemmer.add(w.toCharArray(), w.length());
             stemmer.stem();
             stemmedWord = stemmer.toString();
-            if (tfs.containsKey(stemmedWord)){
+            if (tfs.containsKey(stemmedWord)) {
                 tfs.put(stemmedWord, tfs.get(stemmedWord) + 1);
             } else {
                 tfs.put(stemmedWord, 1);
             }
         }
 
-        //find max
-
-        int maxVal = -1;
-        for (Map.Entry<String, Integer> e : tfs.entrySet()){
-            if (e.getValue() > maxVal){
+        // Find the maximum term frequency.
+        int maxVal = Integer.MIN_VALUE;
+        for (Map.Entry<String, Integer> e : tfs.entrySet()) {
+            if (e.getValue() > maxVal) {
                 maxVal = e.getValue();
             }
         }
 
-        Pattern p = Pattern.compile("[0-9]+");
-        Pattern p2 = Pattern.compile("[0-9]+[\\p{Alnum}]+]");
-        Pattern p3 = Pattern.compile("[\\p{Alpha}]+[0-9]\\p{Alnum}]*");
-        double tf;
-        for (String w : tfs.keySet()){
-            if(!stopWords.contains(w)) {
-                if (w.length() > 4 && (p.matcher(w).matches() || p2.matcher(w).matches() || p3.matcher(w).matches())){
-                    continue;
-                }
-                Text word = new Text();
-                Text textUrl = new Text();
-                word.set(w);
-                //textUrl.set(url);
+        // Patterns that we should not be matching
+        final Pattern p = Pattern.compile("[0-9]+");
+        final Pattern p2 = Pattern.compile("[0-9]+[\\p{Alnum}]+]");
+        final Pattern p3 = Pattern.compile("[\\p{Alpha}]+[0-9]\\p{Alnum}]*");
 
-                tf = .5 + (.5 * (double) tfs.get(w) / maxVal);
-                textUrl.set(new Double(tf).toString() + "," + url);
-                c.write(word, textUrl);
+        for (String w : tfs.keySet()) {
+            if (p.matcher(w).matches()
+                    || p2.matcher(w).matches()
+                    || p3.matcher(w).matches()) {
+                continue;
+            }
+
+            // Calculate the term frequency and write it out.
+            if (!stopWords.contains(w)) {
+                double tf = .5 + (.5 * (double) tfs.get(w) / maxVal);
+                c.write(new Text(w), new Text(tf + "," + url));
             }
         }
-
-
-       // S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
-
-
-//        StringTokenizer tokenizer = new StringTokenizer(line);
-//        while (tokenizer.hasMoreTokens()) {
-//            word.set(tokenizer.nextToken());
-//            output.collect(word, one);
-//        }
-
-
-
     }
 }
